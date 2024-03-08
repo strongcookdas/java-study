3fe0a65213cf7d895470d6fcfac333deec0cec4b
strongcookdas <goodjav141592@gmail.com>
2024-03-08 21:51:01 +0900

    chore : 기본형 vs 참조형3

---
+++ java-basic/src/ref/MethodChange2.java
@@ -0,0 +1,15 @@
+package ref;
+
+public class MethodChange2 {
+    public static void main(String[] args) {
+        Data dataA = new Data();
+        dataA.value = 10;
+        System.out.println("메소드 호출 전: " + dataA.value);
+        changeReference(dataA);
+        System.out.println("메소드 호출 후: " + dataA.value);
+    }
+
+    private static void changeReference(Data data) {
+        data.value = 20;
+    }
+}
